fe3431ad6ca9215d3f254208c992b851502a86bd
SrWillxD <willgra09@hotmail.com>
2023-08-20 16:48:27 -0300

    General structure of the project

---
+++ src/Routes/animals.route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const routes = express.Router();
+
+routes.get("/", async(req, res)=>{
+    try {
+        
+    }catch (err) {
+        
+    }
+});
+
+export default routes;
---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+import cors from 'cors';
+import animalsRoute from './Routes/animals.route.js';
+import ownersRoute from './Routes/owners.route.js';
+
+const port = 3333;
+app.use(cors());
+app.use(express.json());
+
+app.use('/owners', ownersRoute);
+app.use('/animals', animalsRoute);
+
+
+
+
+app.listen(3333, () => console.log(`⚡🚪 Backend started at http://localhost:${port}`));
