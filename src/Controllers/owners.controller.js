4f94bba05b2f3b80cbe1ee4b6e0cff9614ad893b
SrWillxD <willgra09@hotmail.com>
2023-08-27 21:07:53 -0300

    Adding a route to add an owner

--- src/Controllers/owners.controller.js
+++ src/Controllers/owners.controller.js
@@ -19,6 +19,36 @@ const ownersControllerOBJ = {
                 client.release();
             }
         }
+    },
+    async makeOwnersData(req, res, next){
+        let client;
+        try{
+            const { name, tell } = req.body;
+
+            if (!name || !tell) {
+                return res.status(400).json({ error: "Name and tell are required fields." });
+            }
+    
+            if (typeof name !== 'string' || typeof tell !== 'string') {
+                return res.status(400).json({ error: "Name and tell must be strings." });
+            }
+            
+            client = await pool.connect();
+
+            const query = ' INSERT INTO owners (name, tell) VALUES ($1, $2) RETURNING *';
+            const result = await client.query(query, [name, tell]);
+            
+            const insertedOwner = result.rows[0];
+            
+            res.status(201).json(insertedOwner);
+        }catch(err){
+            console.error("Erro ao cadastrar um novo dono:", err);
+            res.status(500).json({ error: "Erro ao cadastrar um novo dono", err });
+        }finally{
+            if(client){
+                client.release();
+            }
+        }
     }
 
 
--- src/Routes/owners.route.js
+++ src/Routes/owners.route.js
@@ -3,5 +3,6 @@ const routes = express.Router();
 import ownersControllerOBJ from '../Controllers/owners.controller.js';
 
 routes.get("/getowners", ownersControllerOBJ.getOwnersData);
+routes.post("/makeowners", ownersControllerOBJ.makeOwnersData);
 
 export default routes;
